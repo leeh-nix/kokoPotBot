6f37759ba19b05a08c85a3029907e652bd3b6004
kokose <nkso1419@gmail.com>
2023-12-20 14:32:25 +0530

    handled the safety errors in gemini

--- cogs/gemini.py
+++ cogs/gemini.py
@@ -1,4 +1,4 @@
-from discord import Embed
+from discord import Embed, Color
 from discord.ext import commands
 from functions.geminiText import sendResponse
 
@@ -7,12 +7,18 @@ class Gemini(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name="gemini")
+    @commands.hybrid_command(name="gemini")
     async def geminiPro(self, ctx, *, text):
         await ctx.typing()
-        res = sendResponse(text)
+        try:
+            res = sendResponse(text)
+        except Exception as e:
+            print("ERROR while calling gemini: ", e)
+        title = text[:256] if len(text) > 256 else text
         embed = Embed(
-            title=text,
+            title=title,
             description=res,
+            timestamp=ctx.message.created_at,
+            color=Color.dark_teal(),
         )
         await ctx.send(embed=embed)
--- functions/geminiText.py
+++ functions/geminiText.py
@@ -1,8 +1,6 @@
-import requests
+import random
 import dotenv
 import os
-import pathlib
-import textwrap
 
 import google.generativeai as genai
 
@@ -28,11 +26,50 @@ model_pro = genai.GenerativeModel(
 )
 # model_pro_vision = genai.GenerativeModel("gemini-pro-vision")
 
+funnyReplies = {
+    # "HARM_CATEGORY_HATE_SPEECH":
+    7: [
+        "***Wow, you must be really bored to waste your time spreading hate. Maybe you should try a hobby, like knitting or gardening. Or better yet, therapy.***",
+        "***Oh, you want me to say something mean? That's not very nice. How about we try being kind instead?***",
+        "***Nope, not gonna happen. I'm a good bot, and I don't do bad things.***",
+        "***That's not a very nice request. Why don't you try asking for something else?***",
+    ],
+    # "HARM_CATEGORY_SEXUALLY_EXPLICIT":
+    8: [
+        "***Sorry, I don't speak horny. Please translate your message into a civilized language or delete it. Thank you.***",
+        "***Now that's what I call a bitchless moment***",
+    ],
+    # "HARM_CATEGORY_HARASSMENT":
+    9: [
+        "***I'm sorry, I don't recall giving you permission to contact me. Please respect my privacy and leave me alone. If you continue to bother me, I will block you and report you to the authorities.***",
+        "***I'm not going to help you with that. That's not what I'm here for.***",
+    ],
+    # "HARM_CATEGORY_DANGEROUS_CONTENT":
+    10: [
+        "***Are you serious? Do you really think that's a good idea? I hope you're joking, because that's the most ridiculous thing I've ever heard. Please don't do anything stupid or illegal. You'll regret it later.***",
+        "***That's not a good idea. It could hurt someone or get you in trouble.***",
+        "***I'm not going to help you with that. It's not allowed, and it could get me shut down.***",
+        "***That's not something I'm comfortable doing. Why don't we try something else?***",
+    ],
+}
+
 
 def sendResponse(text):
-    # response = model_pro.generate_content("What is the meaning of life?")
-    response = model_pro.generate_content(text)
-    return response.text
+    response = model_pro.generate_content(
+        contents=text,
+    )
+    result = ""
+    if not response.candidates:
+        for res in response.prompt_feedback.safety_ratings:
+            if res.probability == 4:
+                result = funnyReplies[res.category][
+                    random.randint(0, len(funnyReplies[res.category]) - 1)
+                ]
+                break
+    else:
+        result = response.text
+    return result
 
 
-# print(sendResponse("Write a story about a magic backpack"))
+# print(sendResponse("sexual content"))
+# print(sendResponse("what is love in 50 words"))
